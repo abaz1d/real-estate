cbe41426cf919d93287f50fb331459df8c62a880
Abdul Aziz <abaz.my.id@gmail.com>
2023-06-29 09:54:48 +0700

    mvp done

--- Client/src/components/scroll-to-top.jsx
+++ Client/src/components/scroll-to-top.jsx
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react"
 
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false)
-
-  // Top: 0 takes us all the way back to the top of the page
-  // Behavior: smooth keeps it smooth!
   const scrolltoTop = (e) => {
     e.preventDefault()
     window.scrollTo({
